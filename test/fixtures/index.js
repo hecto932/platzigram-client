943b0eb514d760becbdf6e908dd8f10fe77d44b9
Hector Flores <hecto932@gmail.com>
2016-11-06 15:48:28 -0600

    Agregando .gitignore y Creando nuestro cliente

---
+++ test/fixtures/index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const uuid = require('uuid-base62')
+
+const fixtures = {
+  getImage () {
+    let id = uuid.uuid()
+    return {
+      description: 'an #awesome picture with #tags #platzi',
+      tags: ['awesome', 'tags', 'platzi'],
+      url: `https://platzigram.test/${uuid.v4()}.jpg`,
+      likes: 0,
+      liked: false,
+      userId: uuid.encoded(id),
+      id: id,
+      createdAt: new Date().toString()
+    }
+  },
+
+  getImages (n) {
+    let images = []
+    while (n-- > 0) {
+      images.push(this.getImage())
+    }
+  },
+
+  getUser () {
+    return {
+      id: uuid.uuid(),
+      name: 'A random user',
+      username: 'platzigram',
+      createdAt: new Date().toString()
+    }
+  }
+}
+
+module.exports = fixtures
